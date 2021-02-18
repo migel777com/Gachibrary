a3a307923504406f9d33e5364138fd7b0d6ff05f
migel777com <62992979+migel777com@users.noreply.github.com>
2021-02-19 03:46:23 +0600

    Profile Page

--- src/main/java/kz/edu/controller/Controller2.java
+++ src/main/java/kz/edu/controller/Controller2.java
@@ -4,6 +4,9 @@ import kz.edu.dao.UserDAO;
 import kz.edu.model.Role;
 import kz.edu.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,8 +39,16 @@ public class Controller2 {
     }
 
     @GetMapping("/profile")
-    public String profile()
+    public String profile(Model model)
     {
+        String currentUserName = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            currentUserName = authentication.getName();
+        }
+        User user = userDAO.findByUserName(currentUserName);
+        model.addAttribute("user", user);
+
         return "profile";
     }
 
